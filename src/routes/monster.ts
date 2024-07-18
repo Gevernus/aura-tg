7238501c9b5d0f41fdfa2d45215a2828f77eae13
Artem Povaliaev <gevernus@mail.ru>
2024-07-18 04:41:25 +0400

    Added shop with passive income

--- src/routes/monster.ts
+++ src/routes/monster.ts
@@ -39,17 +39,21 @@ router.post('/:userId/monsters/upgrade/:monsterId', async (req, res) => {
         if (userMonster) {
             const cardConfig = config.cardConfigs[userMonster.monster.rarity]
             const price = Math.round(cardConfig.basePrice * Math.pow(1.30, userMonster.level))
-            const state = await State.findOne({ where: { id: userId } })
-            if (state) {
-                userMonster.level += 1;
-                state.coins -= price;
-                await userMonster.save();
-                await state.save();
-                console.log(`Upgraded UserMonster: ${JSON.stringify(userMonster)}`);
-                res.status(200).json({ message: 'Monster upgraded successfully', userMonster, coins: state.coins });
-            } else {
-                res.status(200).json({ message: 'Not enough coins to buy', userMonster });
-            }
+            // const state = await State.findOne({ where: { id: userId } })
+            userMonster.level += 1;
+            await userMonster.save();
+            console.log(`Upgraded UserMonster: ${JSON.stringify(userMonster)}`);
+            res.status(200).json({ message: 'Monster upgraded successfully', userMonster });
+            // if (state) {
+            //     userMonster.level += 1;
+            //     state.coins -= price;
+            //     await userMonster.save();
+            //     await state.save();
+            //     console.log(`Upgraded UserMonster: ${JSON.stringify(userMonster)}`);
+            //     res.status(200).json({ message: 'Monster upgraded successfully', userMonster, coins: state.coins });
+            // } else {
+            //     res.status(200).json({ message: 'Not enough coins to buy', userMonster });
+            // }
         } else {
             console.log(`UserMonster not found for userId: ${userId} and monsterId: ${monsterId}`);
             res.status(404).json({ error: 'UserMonster not found' });
