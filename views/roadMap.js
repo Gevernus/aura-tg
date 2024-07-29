030ad7e054193458839394df5bd9374af88d30d7
Artem Povaliaev <gevernus@mail.ru>
2024-07-29 20:54:24 +0400

    Added road map page

---
+++ views/roadMap.js
@@ -0,0 +1,8 @@
+import { CoinsComponent, PassiveIncomeComponent, ClickPowerComponent, TasksComponent, UserComponent } from '../dist/components.js';
+
+export function init(entity) {
+};
+
+export function render(entity) {
+
+}
