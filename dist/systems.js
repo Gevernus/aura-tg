462ea9d3087bf69a84f7044405ec871a8c6c26e9
Artem Povaliaev <gevernus@mail.ru>
2024-08-06 18:36:27 +0400

    Changed haptic feedback method

--- dist/systems.js
+++ dist/systems.js
@@ -209,7 +209,8 @@ export class TelegramSystem extends System {
 
         const vibrate = inputComponent.getAndRemoveInputs('vibrate');
         if (vibrate && vibrate.length > 0) {
-            window.Telegram.WebApp.HapticFeedback.impactOccurred('soft');
+            // window.Telegram.WebApp.HapticFeedback.impactOccurred('soft');
+            window.Telegram.WebApp.HapticFeedback.selectionChanged();
             // window.Telegram.WebApp.HapticFeedback.notificationOccurred('warning');
         }
     }
