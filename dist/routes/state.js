6f7fa9711378ae3f6ec9b904ef7dabc8d310757a
Artem Povaliaev <gevernus@mail.ru>
2024-08-07 19:58:29 +0400

    Exclude users from rating

--- dist/routes/state.js
+++ dist/routes/state.js
@@ -156,6 +156,7 @@ router.get('/:userId/inventory', async (req, res) => {
 });
 router.get('/ratings', async (req, res) => {
     try {
+        const excludedUsernames = ['farequest312', 'vvvvvvv300300'];
         const ratings = await User_1.User
             .createQueryBuilder("user")
             .leftJoinAndSelect("user.referrals", "referral")
@@ -170,6 +171,7 @@ router.get('/ratings', async (req, res) => {
             "SUM(DISTINCT COALESCE(referralState.passive_income, 0)) AS friendsPassiveIncome",
             "AVG(DISTINCT COALESCE(referralState.level, 1)) AS friendsAverageLevel",
         ])
+            .where("user.username NOT IN (:...excludedUsernames)", { excludedUsernames })
             .groupBy("user.id, user.username, userState.coins, userState.passive_income")
             .getRawMany();
         res.status(200).json(ratings);
--- src/routes/state.ts
+++ src/routes/state.ts
@@ -164,6 +164,7 @@ router.get('/:userId/inventory', async (req, res) => {
 
 router.get('/ratings', async (req, res) => {
     try {
+        const excludedUsernames = ['farequest312', 'vvvvvvv300300'];
         const ratings = await User
             .createQueryBuilder("user")
             .leftJoinAndSelect("user.referrals", "referral")
@@ -178,6 +179,7 @@ router.get('/ratings', async (req, res) => {
                 "SUM(DISTINCT COALESCE(referralState.passive_income, 0)) AS friendsPassiveIncome",
                 "AVG(DISTINCT COALESCE(referralState.level, 1)) AS friendsAverageLevel",
             ])
+            .where("user.username NOT IN (:...excludedUsernames)", { excludedUsernames })
             .groupBy("user.id, user.username, userState.coins, userState.passive_income")
             .getRawMany();
 
